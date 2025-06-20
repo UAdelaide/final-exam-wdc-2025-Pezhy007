83a04c37ed45d5fa5b9561dae1517eb0370c2a72
Pezhy <pezhmanbeheshtian@gmail.com>
2025-06-20 15:24:06 +0000

    [automatic commit] 2025-06-20 15:24:06 UTC 115.166.25.152

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -60,7 +60,11 @@ router.post('/login', async (req, res) => {
 
 // POST logout
 router.post('/logout', (req, res) => {
-  req.session.destroy((err) =>)
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status
+    }
+  })
 })
 
 module.exports = router;
